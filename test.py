69b80ce7e8f7b786b1ee1bdc4c5e8e8d06cea755
coherent17 <mnb51817@gmail.com>
2021-04-13 13:54:22 +0800

    test

--- test.py
+++ test.py
@@ -10,12 +10,14 @@ dataT=np.genfromtxt("t.csv",delimiter=',')
 def normalization(dataX):
     for i in range(0,len(dataX)):
         dataX[i]=(dataX[i]-2*i/3)/0.1
+    return dataX
 
 def shuffle(dataX,dataT):
     data_temp=np.c_[dataT,dataX]
     np.random.shuffle(data_temp)
     dataT=data_temp[:,0]
     dataX=np.delete(data_temp,[0],axis=1)
+    return dataX,dataT
 
 def sigmoidal(dataX):
     for i in range(0,len(dataX)):
@@ -26,18 +28,29 @@ def linear_regression(X,Y):
     w=np.matmul(np.matmul(np.linalg.pinv(np.matmul(X.T,X)),X.T),Y)
     return w
 
+def hypothesis(w,X):
+    return np.matmul(w,np.transpose(X))
 
-shuffle(dataX,dataT)
+dataX,dataT=shuffle(dataX,dataT)
+
 #N=5
-dataX_sig=dataX
-normalization(dataX_sig)
+dataX_sig=np.copy(dataX)
+dataX_sig=normalization(dataX_sig)
 dataX_sig=sigmoidal(dataX_sig)
 dataX_sig=np.c_[np.array([1]*len(dataX_sig)),dataX_sig]
 #take 5 points to train
 w=linear_regression(dataX_sig[0:5],dataT[0:5])
 
-#visualize:
-x=np.linspace(0,2,20000)
-y=w[0]+w[1]*x
-plt.plot(x,y)
+#generate the testing dataset of x
+#need to do data preprocessing as same as the given x data
+x=np.linspace(0,2,200)
+x_sig=np.copy(x)
+x_sig=normalization(x_sig)
+x_sig=sigmoidal(x_sig)
+x_sig=np.c_[np.array([1]*len(x_sig)),x_sig]
+y=hypothesis(w,x_sig)
+
+#visualize
+plt.plot(x,y,color="r")
+plt.plot(dataX[:],dataT[:],'bo')
 plt.show()
